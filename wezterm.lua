3f19e0ad4c5e043eb50b37ad62c1b4db1fb699d7
zyosaidouki <70832246+zyosaidouki@users.noreply.github.com>
2024-11-10 19:18:39 +0900

    fix

--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- config.color_scheme = 'Batman'
+config.color_scheme = 'Galaxy'
 config.window_background_opacity = 0.6
 config.macos_window_background_blur = 20
 
