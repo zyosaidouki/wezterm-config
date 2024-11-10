a866f449e385f0e74a592e3e260ead1c45a00ca2
zyosaidouki <70832246+zyosaidouki@users.noreply.github.com>
2024-11-10 16:11:55 +0900

    change

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,6 @@ local wezterm = require 'wezterm'
 local config = {}
 
 -- config.color_scheme = 'Batman'
---config.window_background_opacity = 0.7
 config.window_background_opacity = 1.0
 
 config.keys = {
