05ced976cc704c141a5b0e8f9aae6dbefd1dc36a
zyosaidouki <70832246+zyosaidouki@users.noreply.github.com>
2024-11-14 19:42:30 +0900

    fix

--- wezterm.lua
+++ wezterm.lua
@@ -4,7 +4,7 @@ local config = {}
 -- config.color_scheme = 'Batman'
 config.color_scheme = 'Galaxy'
 config.window_background_opacity = 0.6
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 15
 
 config.keys = {
   -- CMD-y starts `top` in a new window
